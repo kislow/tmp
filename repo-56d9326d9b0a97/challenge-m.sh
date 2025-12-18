73329ea6fc0106c28f0c5f8509842ac60d05c258
sudo-suadKF <suadfehric@outlook.com>
2025-12-18 20:09:12 +0100

    Added some code, checking that all required file exist before they are used and checked that all required input files are not empty.

--- repo-56d9326d9b0a97/challenge-m.sh
+++ repo-56d9326d9b0a97/challenge-m.sh
@@ -7,6 +7,27 @@ BACKUP_DIR="/var/tmp/m-backups"
 BACKUP_FILE="backup.txt"
 SOURCE_FILE="data/users.txt"
 
+if [[ ! -f "$BACKUP_FILE" ]]; then
+  echo "required file '$FILE' does not exist"
+  touch "$BACKUP_FILE"
+fi
+
+if [[ ! -d "$BACKUP_DIR" ]]; then
+  echo " Backup directory '$BACKUP_DIR' does not exist. Creating it."
+  mkdir -p "$BACKUP_DIR"
+fi
+
+if [[ ! -s $SOURCE_FILE ]]; then
+    echo "$SOURCE_FILE is not empty!"
+fi
+
+if [[ ! -s $BACKUP_FILE ]]; then
+    echo "$BACKUP_FILE is not empty!"
+else
+    echo "Add text" > $BACKUP_FILE
+    echo "$BACKUP_FILE was empty, added text"
+fi
+
 echo "Starting male challenge backup..."
 
 # Copy users file into backup directory
